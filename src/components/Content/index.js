851b4935adc8dd4be9035cad0e9e20fa106ff802
lucasokano <luc4sko@hotmail.com>
2021-05-24 14:25:14 -0300

    estrutura básica

---
+++ src/components/Content/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Content = props => {
+    return(
+        <div className="Content">
+            <h1>Conteúdo</h1>
+            <p>lalalalalalalalalalalala</p>
+        </div>
+    )
+    
+ }
+
+export default Content;
---
+++ src/components/Menu/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Menu = props => {
+    return(
+        <nav>
+            <ul>
+                <li></li>
+                <li></li>
+            </ul>
+        </nav>
+
+
+
+    )
+ }
+
+export default Menu;
