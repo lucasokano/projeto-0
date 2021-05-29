6695c4ac376afc2dacabc4a28ac6310cf838a867
lucasokano <luc4sko@hotmail.com>
2021-05-29 20:56:55 -0300

    pages viraram components proprios

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -2,23 +2,20 @@ import React from 'react'
 import './index.css'
 import {Switch, Route} from 'react-router-dom'
 
+import Home from '../../views/Home'
+import About from '../../views/About'
+
 const Content = props => {
     return(
         <div className="Content">
             <Switch>
                 <Route path="/Home">
-                    <h1>Home</h1>
-                    <p>aqui está o texto do Home</p>
+                    <Home/>
                 </Route>
 
                 <Route path="/About">
-                    <h1>About this fucking project</h1>
-                    <p>Aqui está o texto do About</p>
+                    <About/>
                 </Route>
-
-
-
-
             </Switch>
         </div>
     )
